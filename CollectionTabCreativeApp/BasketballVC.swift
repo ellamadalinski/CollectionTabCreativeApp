019130e7113b28f8cac5118ddfd6efc297b7161a
Ella Madalinski <ella.madalinski24@d155.org>
2021-11-09 08:09:30 -0600

    finishing soccer

--- CollectionTabCreativeApp/BasketballVC.swift
+++ CollectionTabCreativeApp/BasketballVC.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
-class BasketballVC: UIViewController {
+class BasketballVC: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout{
 
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
+    
+    var basketball : [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
+        
+        basketball.append(UIImage(named: "basketball1")!)
+        basketball.append(UIImage(named: "basketball2")!)
+        basketball.append(UIImage(named: "basketball3")!)
+        basketball.append(UIImage(named: "basketball4")!)
+        basketball.append(UIImage(named: "basketball5")!)
+        basketball.append(UIImage(named: "basketball6")!)
+        basketball.append(UIImage(named: "basketball7")!)
+        basketball.append(UIImage(named: "basketball8")!)
+        basketball.append(UIImage(named: "basketball9")!)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let yourWidth = collectionView.bounds.width/3.0
+        let yourHeight = yourWidth
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        return CGSize(width: yourWidth, height: yourHeight)
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return basketball.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionViewOutlet.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath)
+        cell.contentView.addSubview(UIImageView(image: basketball[indexPath.row]))
+        return cell
+    }
 
 }
